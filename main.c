0e8500c3c5839304066502f02b55e168b7cf8f4d
CaptainPotatoes <musasmahmood@gmail.com>
2017-04-03 12:30:03 -0400

    LEDs for pins 2 & 3 on the full board. Indicate power on/advertising & bluetooth.

--- main.c
+++ main.c
@@ -448,6 +448,10 @@ static void on_ble_evt(ble_evt_t * p_ble_evt)
 					#if defined(BOARD_NRF_BREAKOUT)
 						nrf_gpio_pin_set(13);
 					#endif
+					#if defined(BOARD_FULL_EEG_V1)
+						nrf_gpio_pin_clear(3);
+						nrf_gpio_pin_set(2);
+					#endif
             m_conn_handle = p_ble_evt->evt.gap_evt.conn_handle;
             break;
 
@@ -457,6 +461,10 @@ static void on_ble_evt(ble_evt_t * p_ble_evt)
 						#endif
 				#if defined(BOARD_NRF_BREAKOUT)
 						nrf_gpio_pin_clear(13);
+				#endif
+				#if defined(BOARD_FULL_EEG_V1)
+						nrf_gpio_pin_clear(2);
+						nrf_gpio_pin_set(3);
 				#endif
             m_conn_handle = BLE_CONN_HANDLE_INVALID;
             break;
@@ -684,6 +692,10 @@ static void ads1299_gpio_init(void) {
 		nrf_gpio_cfg_output(13);
 		nrf_gpio_cfg_output(14);
 	#endif
+	#if defined(BOARD_FULL_EEG_V1)
+		nrf_gpio_cfg_output(3);
+		nrf_gpio_cfg_output(2);
+	#endif
 	#if defined(BOARD_NRF_BREAKOUT) | defined(BOARD_PCA10028)
 		nrf_gpio_pin_dir_set(ADS1299_DRDY_PIN, NRF_GPIO_PIN_DIR_INPUT); //sets 'direction' = input/output
 		nrf_gpio_pin_dir_set(ADS1299_PWDN_RST_PIN, NRF_GPIO_PIN_DIR_OUTPUT);
@@ -762,6 +774,10 @@ int main(void)
 		int32_t eeg4;
 		#if defined(BOARD_NRF_BREAKOUT)
 			nrf_gpio_pin_set(14);
+		#endif
+		#if defined(BOARD_FULL_EEG_V1) //Board Ready, (GreenLight)
+			nrf_gpio_pin_clear(2);
+			nrf_gpio_pin_set(3);
 		#endif
     // Enter main loop.
     for (;;) {
